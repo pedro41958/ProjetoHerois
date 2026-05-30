5c11c7c24c686f7ac5413f32976c3d29e8c2c257
Pedro Oliveira <pedro41958@fiec.edu.br>
2026-05-30 10:45:51 -0300

    zod backend

--- backend/src/controllers/heroicontroller.js
+++ backend/src/controllers/heroicontroller.js
@@ -1,7 +1,15 @@
+const { success } = require("zod");
 const db = require("../config/db");
+const schema = require("../schemas/heroischema");
 
 exports.cadastarHeroi = async (req, res) => {
-  const { nome, classe, poder, status } = req.body;
+  const resultado = schema.safeParse(req.body);
+
+  if (!resultado.success) {
+    return res.status(400).json(resultado.error.issues);
+  }
+
+  const { nome, classe, poder, status } = resultado.data;
 
   try {
     const cadastarHeroi = await db.query(

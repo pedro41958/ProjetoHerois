72796de601784b89f13c516aea6b95ca43364daa
Pedro Oliveira <pedro41958@fiec.edu.br>
2026-06-17 21:47:52 -0300

    fazendo "perfil"

--- frontend/src/components/Perfil.jsx
+++ frontend/src/components/Perfil.jsx
@@ -4,6 +4,11 @@ function Perfil() {
       <h1 className="bg-slate-500 text-white w-full text-center p-2 mb-6 font-semibold">
         Bem-vindo treinador!
       </h1>
+      <div className="flex flex-col bg-white p-6 rounded-xl shadow-md w-80 border-4 border-gray-400">
+        <h1>Perfil do Treinador:</h1>
+        <p>NOME:</p>
+        <p>E-MAIL:</p>
+      </div>
     </div>
   );
 }

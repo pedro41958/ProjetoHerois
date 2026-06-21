ac3cfd8dcc55ef9e9c720f4a6e9da6d596723e36
Pedro Oliveira <pedro41958@fiec.edu.br>
2026-06-21 11:31:35 -0300

    tabelas banco de dados

--- banco.sql
+++ banco.sql
@@ -8,3 +8,33 @@ CREATE TABLE usuarios (
   email varchar(100) NOT NULL,
   senha varchar(100) NOT NULL
 );
+
+CREATE TABLE herois (
+  id_heroi INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL,
+  classe ENUM('Sprint', 'Mile', 'Medium', 'Long') NOT NULL,
+  poder INT NOT NULL,
+  url_imagem VARCHAR(255),
+
+  id_usuario INT NOT NULL,
+  id_guilda INT NOT NULL,
+
+  FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
+  FOREIGN KEY (id_guilda) REFERENCES guildas(id_guilda)
+);
+
+CREATE TABLE guildas (
+  id_guilda INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE missoes (
+  id_missao INT AUTO_INCREMENT PRIMARY KEY,
+  descricao VARCHAR(255) NOT NULL,
+  status ENUM('Em andamento', 'Concluida', 'Falhou') NOT NULL,
+  recompensa_ouro INT NOT NULL,
+
+  id_heroi INT NOT NULL,
+
+  FOREIGN KEY (id_heroi) REFERENCES herois(id_heroi)
+);

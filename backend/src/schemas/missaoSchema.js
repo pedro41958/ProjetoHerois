49a649944a2c0f7f168615abf40ee6030e907964
Pedro Oliveira <pedro41958@fiec.edu.br>
2026-06-23 15:16:30 -0300

    ajuste schemaMissao

--- backend/src/schemas/missaoSchema.js
+++ backend/src/schemas/missaoSchema.js
@@ -1,9 +1,9 @@
 const z = require("zod");
 
 const schemaCriarMissao = z.object({
-  nome: z.string().min(3, "Mínimo 3 caracteres!"),
+  descricao: z.string().min(3, "Mínimo 3 caracteres!"),
   status: z.enum(["Em andamento", "Concluida", "Falhou"], "Classe inválida!"),
-  poder: z.coerce.number().min(0, "Mínimo poder 0!"),
+  recompensa_ouro: z.coerce.number().min(0, "Mínimo poder 0!"),
   id_heroi: z.coerce.number().min(1, "Escolha uma Umamusume!"),
 });
 
